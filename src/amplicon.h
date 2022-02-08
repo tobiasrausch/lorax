b64316e2059fe2926a59179fc1691683e798006f
tobiasrausch <rauschtobi@gmail.com>
2022-02-08 14:47:18 +0100

    split-read junctions

--- src/amplicon.h
+++ src/amplicon.h
@@ -80,77 +80,22 @@ namespace lorax
       dataOut << ">" << bam_get_qname(rec) << std::endl;
       dataOut << sequence << std::endl;
     }
+    // Clean-up
+    bam_destroy1(rec);
+    
     // Close output file
     dataOut.pop();
     dataOut.pop();
     
     // Clean-up
-    bam_destroy1(rec);
     bam_hdr_destroy(hdr);
     sam_close(samfile);
-  }  
-      
-    
-  template<typename TConfig>
-  inline int32_t
-  depleteReads(TConfig const& c, std::set<std::size_t>& candidates) {
-    // Open file handles
-    samFile* samfile = sam_open(c.tumor.string().c_str(), "r");
-    hts_set_fai_filename(samfile, c.genome.string().c_str());
-    hts_idx_t* idx = sam_index_load(samfile, c.tumor.string().c_str());
-    bam_hdr_t* hdr = sam_hdr_read(samfile);
-    
-    // Parse amplicons
-    typedef boost::icl::interval_set<uint32_t> TChrIntervals;
-    typedef std::vector<TChrIntervals> TRegionsGenome;
-    TRegionsGenome scanRegions;
-    _parseBedIntervals(c.bedfile.string(), hdr, scanRegions);
-
-    // Deplete reads
-    for (int refIndex = 0; refIndex<hdr->n_targets; ++refIndex) {
-      // Any amplicon regions?
-      if (scanRegions[refIndex].empty()) continue;
-      
-      // Assign reads to haplotypes
-      for(typename TChrIntervals::iterator it = scanRegions[refIndex].begin(); it != scanRegions[refIndex].end(); ++it) {
-	if ((it->lower() < it->upper()) && (it->lower() >= 0) && (it->upper() <= hdr->target_len[refIndex])) {
-	  boost::posix_time::ptime now = boost::posix_time::second_clock::local_time();
-	  std::cout << '[' << boost::posix_time::to_simple_string(now) << "] Processing " << hdr->target_name[refIndex] << ':' << it->lower() << '-' << it->upper() << std::endl;
-
-	  std::vector<uint32_t> bps;
-	  if (it->lower() > c.bpuncertain) bps.push_back(it->lower() - c.bpuncertain);
-	  else bps.push_back(0);
-	  if (it->upper() + c.bpuncertain < hdr->target_len[refIndex]) bps.push_back(it->upper() + c.bpuncertain);
-	  else bps.push_back(hdr->target_len[refIndex] - 1);
-	  for(uint32_t bpi = 0; bpi < bps.size(); ++bpi) {
-	    hts_itr_t* iter = sam_itr_queryi(idx, refIndex, bps[bpi], bps[bpi] + 1);
-	    bam1_t* rec = bam_init1();
-	    while (sam_itr_next(samfile, iter, rec) >= 0) {
-	      if (rec->core.flag & (BAM_FSECONDARY | BAM_FQCFAIL | BAM_FDUP | BAM_FUNMAP)) continue;
-	      if ((rec->core.qual < c.minQual) || (rec->core.tid<0)) continue;
-	      std::size_t seed = hash_string(bam_get_qname(rec));
-	      candidates.erase(seed);
-	    }
-	    bam_destroy1(rec);
-	    hts_itr_destroy(iter);	  
-	  }
-	}
-      }
-    }
-    
-    // Clean-up
-    bam_hdr_destroy(hdr);
-    hts_idx_destroy(idx);
-    sam_close(samfile);
-
-    return candidates.size();
   }
-
-  
-
+      
+    
   template<typename TConfig>
   inline int32_t
-  selectReads(TConfig const& c, std::set<std::size_t>& amp_reads) {
+  selectReads(TConfig const& c, std::set<std::size_t>& candidates) {
     // Open file handles
     samFile* samfile = sam_open(c.tumor.string().c_str(), "r");
     hts_set_fai_filename(samfile, c.genome.string().c_str());
@@ -175,6 +120,14 @@ namespace lorax
       std::cout << '[' << boost::posix_time::to_simple_string(now) << "] Parsed " << nreg << " amplicon regions." << std::endl;
     }
 
+    // Discarded reads
+    std::set<std::size_t> discarded;
+    
+    // Breakpoint support
+    typedef std::set<uint32_t> TBpLocation;
+    typedef std::map<std::size_t, TBpLocation> TReadClip;
+    TReadClip readClip;
+    
     // Assign reads to SNPs
     faidx_t* fai = fai_load(c.genome.string().c_str());
     for (int refIndex = 0; refIndex<hdr->n_targets; ++refIndex) {
@@ -195,7 +148,7 @@ namespace lorax
       int32_t seqlen = -1;
       char* seq = NULL;
       seq = faidx_fetch_seq(fai, chrName.c_str(), 0, hdr->target_len[refIndex], &seqlen);    
-    
+
       // Assign reads to haplotypes
       for(typename TChrIntervals::iterator it = scanRegions[refIndex].begin(); it != scanRegions[refIndex].end(); ++it) {
 	if ((it->lower() < it->upper()) && (it->lower() >= 0) && (it->upper() <= hdr->target_len[refIndex])) {
@@ -205,92 +158,141 @@ namespace lorax
 	  // Local haplotype assignment
 	  std::set<std::size_t> h1;
 	  std::set<std::size_t> h2;
+
+	  // Breakpoint support
+	  int32_t leftstart = 0;
+	  int32_t leftend = hdr->target_len[refIndex];
+	  if (c.bpuncertain < it->lower()) leftstart = it->lower() - c.bpuncertain;
+	  if (it->lower() + c.bpuncertain < hdr->target_len[refIndex]) leftend = it->lower() + c.bpuncertain;
+	  std::vector<uint16_t> leftBp((leftend - leftstart), 0);      
+	  int32_t rightstart = 0;
+	  int32_t rightend = hdr->target_len[refIndex];
+	  if (c.bpuncertain < it->upper()) rightstart = it->upper() - c.bpuncertain;
+	  if (it->upper() + c.bpuncertain < hdr->target_len[refIndex]) rightend = it->upper() + c.bpuncertain;
+	  std::vector<uint16_t> rightBp((rightend - rightstart), 0);      
 	  
+	  // Scan region
 	  hts_itr_t* iter = sam_itr_queryi(idx, refIndex, it->lower(), it->upper());
 	  bam1_t* rec = bam_init1();
 	  while (sam_itr_next(samfile, iter, rec) >= 0) {
 	    if (rec->core.flag & (BAM_FSECONDARY | BAM_FQCFAIL | BAM_FDUP | BAM_FUNMAP)) continue;
 	    if ((rec->core.qual < c.minQual) || (rec->core.tid<0)) continue;
+	    std::string qname = bam_get_qname(rec);
 	    std::size_t seed = hash_string(bam_get_qname(rec));
-	
-	    uint32_t hp1votes = 0;
-	    uint32_t hp2votes = 0;
-	    int32_t regstart = rec->core.pos;
-	    if (regstart < (int) it->lower()) regstart = it->lower();
-	    TPhasedVariants::iterator vIt = std::lower_bound(pv.begin(), pv.end(), BiallelicVariant(regstart), SortVariants<BiallelicVariant>());
-	    int32_t regend = lastAlignedPosition(rec);
-	    if (regend > (int) it->upper()) regend = it->upper();
-	    TPhasedVariants::iterator vItEnd = std::upper_bound(pv.begin(), pv.end(), BiallelicVariant(regend), SortVariants<BiallelicVariant>());
 
-	    // Any variants?
-	    if (vIt != vItEnd) {
-	      // Get read sequence
-	      std::string sequence;
-	      sequence.resize(rec->core.l_qseq);
-	      uint8_t* seqptr = bam_get_seq(rec);
-	      for (int32_t i = 0; i < rec->core.l_qseq; ++i) sequence[i] = "=ACMGRSVTWYHKDBN"[bam_seqi(seqptr, i)];
-	    
-	      // Parse CIGAR
+
+	    // Split reads
+	    {
 	      uint32_t* cigar = bam_get_cigar(rec);
-	      for(;vIt != vItEnd; ++vIt) {
-		int32_t gp = rec->core.pos; // Genomic position
-		int32_t sp = 0; // Sequence position
-		bool varFound = false;
-		for (std::size_t i = 0; ((i < rec->core.n_cigar) && (!varFound)); ++i) {
-		  if ((bam_cigar_op(cigar[i]) == BAM_CSOFT_CLIP) || (bam_cigar_op(cigar[i]) == BAM_CHARD_CLIP)) {
-		    if (bam_cigar_op(cigar[i]) == BAM_CSOFT_CLIP) sp += bam_cigar_oplen(cigar[i]);
-		    if (bam_cigar_oplen(cigar[i]) > c.minClip) {
-		      // Likely breakpoint-supporting read?
-		      if ((std::abs(gp - (int32_t) it->lower()) < c.bpuncertain) || (std::abs(gp - (int32_t) it->upper()) < c.bpuncertain)) {
-			amp_reads.insert(seed);
-			//std::cerr << seed << ',' << bam_get_qname(rec) << std::endl;
+	      int32_t gp = rec->core.pos; // Genomic position
+	      int32_t sp = 0; // Sequence position
+	      for (std::size_t i = 0; i < rec->core.n_cigar; ++i) {
+		if ((bam_cigar_op(cigar[i]) == BAM_CSOFT_CLIP) || (bam_cigar_op(cigar[i]) == BAM_CHARD_CLIP)) {
+		  if (bam_cigar_oplen(cigar[i]) > c.minClip) {
+		    if (sp == 0) { // Left breakpoint
+		      if (std::abs(gp - (int32_t) it->lower()) < c.bpuncertain) {
+			if (readClip.find(seed) == readClip.end()) readClip[seed] = TBpLocation();
+			readClip[seed].insert(it->lower());
+			if ((gp >= leftstart) && (gp < leftend)) ++leftBp[gp-leftstart];
+		      } else if (std::abs(gp - (int32_t) it->upper()) < c.bpuncertain) {
+			if (readClip.find(seed) == readClip.end()) readClip[seed] = TBpLocation();
+			readClip[seed].insert(it->lower());
+			if ((gp >= rightstart) && (gp < rightend)) ++rightBp[gp-rightstart];
 		      }
-		      
 		    }
 		  }
-		  else if (bam_cigar_op(cigar[i]) == BAM_CINS) sp += bam_cigar_oplen(cigar[i]);
-		  else if (bam_cigar_op(cigar[i]) == BAM_CDEL) gp += bam_cigar_oplen(cigar[i]);
-		  else if (bam_cigar_op(cigar[i]) == BAM_CREF_SKIP) gp += bam_cigar_oplen(cigar[i]);
-		  else if ((bam_cigar_op(cigar[i]) == BAM_CMATCH) || (bam_cigar_op(cigar[i]) == BAM_CEQUAL) || (bam_cigar_op(cigar[i]) == BAM_CDIFF)) {
-		    if (gp + (int32_t) bam_cigar_oplen(cigar[i]) < vIt->pos) {
-		      gp += bam_cigar_oplen(cigar[i]);
-		      sp += bam_cigar_oplen(cigar[i]);
-		    } else {
-		      for(std::size_t k = 0; k<bam_cigar_oplen(cigar[i]); ++k, ++sp, ++gp) {
-			if (gp == vIt->pos) {
-			  varFound = true;
-			  // Check REF allele
-			  if (vIt->ref == seq[gp]) {
-			    if (sp < (int) sequence.size()) {
-			      if ((sequence[sp] == vIt->alt) && (sequence[sp] != vIt->ref)) {
-				// ALT supporting read
-				++vIt->asup;
-				if (vIt->hap) ++hp1votes;
-				else ++hp2votes;
-			      } else if ((sequence[sp] != vIt->alt) && (sequence[sp] == vIt->ref)) {
-				// REF supporting read
-				++vIt->rsup;
-				if (vIt->hap) ++hp2votes;
-				else ++hp1votes;
+		  if (bam_cigar_op(cigar[i]) == BAM_CSOFT_CLIP) sp += bam_cigar_oplen(cigar[i]);
+		}
+		else if (bam_cigar_op(cigar[i]) == BAM_CINS) sp += bam_cigar_oplen(cigar[i]);
+		else if (bam_cigar_op(cigar[i]) == BAM_CDEL) gp += bam_cigar_oplen(cigar[i]);
+		else if (bam_cigar_op(cigar[i]) == BAM_CREF_SKIP) gp += bam_cigar_oplen(cigar[i]);
+		else if ((bam_cigar_op(cigar[i]) == BAM_CMATCH) || (bam_cigar_op(cigar[i]) == BAM_CEQUAL) || (bam_cigar_op(cigar[i]) == BAM_CDIFF)) {
+		  gp += bam_cigar_oplen(cigar[i]);
+		  sp += bam_cigar_oplen(cigar[i]);
+		} else {
+		  std::cerr << "Unknown Cigar options" << std::endl;
+		  return 1;
+		}
+	      }
+	    }
+	    
+
+	    // Amplicon reads
+	    bool ampliconRead = true;
+	    if ((rec->core.pos < it->lower()) && (it->lower() - rec->core.pos > c.bpuncertain)) ampliconRead = false;
+	    uint32_t alignEnd = lastAlignedPosition(rec);
+	    if ((alignEnd > it->upper()) && (alignEnd - it->upper() > c.bpuncertain)) ampliconRead = false;
+	    if (ampliconRead) {
+	      uint32_t hp1votes = 0;
+	      uint32_t hp2votes = 0;
+	      int32_t regstart = rec->core.pos;
+	      if (regstart < (int) it->lower()) regstart = it->lower();
+	      TPhasedVariants::iterator vIt = std::lower_bound(pv.begin(), pv.end(), BiallelicVariant(regstart), SortVariants<BiallelicVariant>());
+	      int32_t regend = alignEnd;
+	      if (regend > (int) it->upper()) regend = it->upper();
+	      TPhasedVariants::iterator vItEnd = std::upper_bound(pv.begin(), pv.end(), BiallelicVariant(regend), SortVariants<BiallelicVariant>());
+
+	      // Any variants?
+	      if (vIt != vItEnd) {
+		// Get read sequence
+		std::string sequence;
+		sequence.resize(rec->core.l_qseq);
+		uint8_t* seqptr = bam_get_seq(rec);
+		for (int32_t i = 0; i < rec->core.l_qseq; ++i) sequence[i] = "=ACMGRSVTWYHKDBN"[bam_seqi(seqptr, i)];
+	    
+		// Parse CIGAR
+		uint32_t* cigar = bam_get_cigar(rec);
+		for(;vIt != vItEnd; ++vIt) {
+		  int32_t gp = rec->core.pos; // Genomic position
+		  int32_t sp = 0; // Sequence position
+		  bool varFound = false;
+		  for (std::size_t i = 0; ((i < rec->core.n_cigar) && (!varFound)); ++i) {
+		    if (bam_cigar_op(cigar[i]) == BAM_CSOFT_CLIP) sp += bam_cigar_oplen(cigar[i]);
+		    else if (bam_cigar_op(cigar[i]) == BAM_CHARD_CLIP) {}
+		    else if (bam_cigar_op(cigar[i]) == BAM_CINS) sp += bam_cigar_oplen(cigar[i]);
+		    else if (bam_cigar_op(cigar[i]) == BAM_CDEL) gp += bam_cigar_oplen(cigar[i]);
+		    else if (bam_cigar_op(cigar[i]) == BAM_CREF_SKIP) gp += bam_cigar_oplen(cigar[i]);
+		    else if ((bam_cigar_op(cigar[i]) == BAM_CMATCH) || (bam_cigar_op(cigar[i]) == BAM_CEQUAL) || (bam_cigar_op(cigar[i]) == BAM_CDIFF)) {
+		      if (gp + (int32_t) bam_cigar_oplen(cigar[i]) < vIt->pos) {
+			gp += bam_cigar_oplen(cigar[i]);
+			sp += bam_cigar_oplen(cigar[i]);
+		      } else {
+			for(std::size_t k = 0; k<bam_cigar_oplen(cigar[i]); ++k, ++sp, ++gp) {
+			  if (gp == vIt->pos) {
+			    varFound = true;
+			    // Check REF allele
+			    if (vIt->ref == seq[gp]) {
+			      if (sp < (int) sequence.size()) {
+				if ((sequence[sp] == vIt->alt) && (sequence[sp] != vIt->ref)) {
+				  // ALT supporting read
+				  ++vIt->asup;
+				  if (vIt->hap) ++hp1votes;
+				  else ++hp2votes;
+				} else if ((sequence[sp] != vIt->alt) && (sequence[sp] == vIt->ref)) {
+				  // REF supporting read
+				  ++vIt->rsup;
+				  if (vIt->hap) ++hp2votes;
+				  else ++hp1votes;
+				}
 			      }
 			    }
 			  }
 			}
 		      }
+		    } else {
+		      std::cerr << "Unknown Cigar options" << std::endl;
+		      return 1;
 		    }
-		  } else {
-		    std::cerr << "Unknown Cigar options" << std::endl;
-		    return 1;
 		  }
 		}
 	      }
-	    }
-	    int32_t hp = 0;
-	    if (hp1votes > 2*hp2votes) hp = 1;
-	    else if (hp2votes > 2*hp1votes) hp = 2;
-	    if (hp) {
-	      if (hp == 1) h1.insert(seed);
-	      else if (hp == 2) h2.insert(seed);
+	      int32_t hp = 0;
+	      if (hp1votes > 2*hp2votes) hp = 1;
+	      else if (hp2votes > 2*hp1votes) hp = 2;
+	      if (hp) {
+		if (hp == 1) h1.insert(seed);
+		else if (hp == 2) h2.insert(seed);
+	      }
 	    }
 	  }
 	  bam_destroy1(rec);
@@ -302,7 +304,8 @@ namespace lorax
 	  TPhasedVariants::const_iterator vIt = std::lower_bound(pv.begin(), pv.end(), BiallelicVariant(it->lower()), SortVariants<BiallelicVariant>());
 	  TPhasedVariants::const_iterator vItEnd = std::upper_bound(pv.begin(), pv.end(), BiallelicVariant(it->upper()), SortVariants<BiallelicVariant>());	  
 	  for(;vIt != vItEnd; ++vIt) {
-	    std::cerr << vIt->pos << ',' << vIt->ref << ',' << vIt->alt << ',' << vIt->hap << ':' << vIt->rsup << ',' << vIt->asup << std::endl;
+	    // Debug code
+	    std::cerr << hdr->target_name[refIndex] << ':' << it->lower() << '-' << it->upper() << ':' << vIt->pos << ',' << vIt->ref << ',' << vIt->alt << ',' << vIt->hap << ':' << vIt->rsup << ',' << vIt->asup << std::endl;
 	    if (vIt->hap) {
 	      h1sup += vIt->asup;
 	      h2sup += vIt->rsup;
@@ -311,22 +314,51 @@ namespace lorax
 	      h2sup += vIt->asup;
 	    }
 	  }
-	  std::cerr << hdr->target_name[refIndex] << ',' << it->lower() << ',' << it->upper() << ':' << h1sup << ',' << h2sup << std::endl;
 
 	  // Select amplicon haplotype
 	  if (h1sup + h2sup > 0) {
-	    if (h1sup > h2sup) {
-	      amp_reads.insert(h1.begin(), h1.end());
-	    } else {
-	      amp_reads.insert(h2.begin(), h2.end());
+	    if (h1sup > 2 * h2sup) {
+	      candidates.insert(h1.begin(), h1.end());
+	      discarded.insert(h2.begin(), h2.end());
+	    } else if (h2sup > 2 * h1sup) {
+	      discarded.insert(h1.begin(), h1.end());
+	      candidates.insert(h2.begin(), h2.end());
 	    }
 	  }
+	  
+	  // Refine breakpoints
+	  int32_t bestLeft = it->lower() - leftstart;
+	  for(uint32_t i = 0; i < leftBp.size(); ++i) {
+	    if (leftBp[i] > leftBp[bestLeft]) bestLeft = i;
+	  }
+	  int32_t bestRight = it->upper() - rightstart;
+	  for(uint32_t i = 0; i < rightBp.size(); ++i) {
+	    if (rightBp[i] > rightBp[bestRight]) bestRight = i;
+	  }
+	  std::cout << '[' << boost::posix_time::to_simple_string(now) << "] Estimated  " << hdr->target_name[refIndex] << ':' << (bestLeft + leftstart) << '-' << (bestRight + rightstart) << std::endl;
 	}
       }
       if (seq != NULL) free(seq);
     }
     fai_destroy(fai);
 
+    // Collect split-reads connecting at least 2 amplicons
+    uint32_t concordant = 0;
+    uint32_t discordant = 0;
+    uint32_t unclear = 0;
+    for(TReadClip::iterator rcit = readClip.begin(); rcit != readClip.end(); ++rcit) {
+      if (rcit->second.size() > 1) {
+	if (candidates.find(rcit->first) != candidates.end()) ++concordant;
+	else if (discarded.find(rcit->first) != discarded.end()) ++discordant;
+	else {
+	  ++unclear;
+	  candidates.insert(rcit->first);
+	}
+      }
+    }
+    std::cout << '[' << boost::posix_time::to_simple_string(boost::posix_time::second_clock::local_time()) << "] Concordant: " << concordant << ", discordant: " << discordant << ", unclear: " << unclear << std::endl;
+
+      
     // Close BCF
     bcf_hdr_destroy(bcfhdr);
     hts_idx_destroy(bcfidx);
@@ -337,7 +369,7 @@ namespace lorax
     hts_idx_destroy(idx);
     sam_close(samfile);
 
-    return amp_reads.size();
+    return candidates.size();
   }
     
 
@@ -355,16 +387,9 @@ namespace lorax
     std::cout << '[' << boost::posix_time::to_simple_string(now) << "] Selecting amplicon reads." << std::endl;
     int32_t candsize = selectReads(c, candidates);
 
-    // Depleting likely normal cell contamination
-    now = boost::posix_time::second_clock::local_time();
-    std::cout << '[' << boost::posix_time::to_simple_string(now) << "] Processing " << candsize << " amplicon reads." << std::endl;
-    int32_t finsize = depleteReads(c, candidates);
-    now = boost::posix_time::second_clock::local_time();
-    std::cout << '[' << boost::posix_time::to_simple_string(now) << "] Depleting " << (candsize - finsize) << " amplicon reads." << std::endl;
-    
     // Searching primary alignments for full sequence
     now = boost::posix_time::second_clock::local_time();
-    std::cout << '[' << boost::posix_time::to_simple_string(now) << "] Fetching sequence information for " << finsize << " amplicon reads." << std::endl;
+    std::cout << '[' << boost::posix_time::to_simple_string(now) << "] Fetching sequence information for " << candsize << " amplicon reads." << std::endl;
     outputReads(c, candidates);
     
 #ifdef PROFILE
@@ -387,7 +412,7 @@ namespace lorax
     generic.add_options()
       ("help,?", "show help message")
       ("quality,q", boost::program_options::value<uint16_t>(&c.minQual)->default_value(10), "min. sequence quality")
-      ("minclip,c", boost::program_options::value<uint32_t>(&c.minClip)->default_value(25), "min. clipping length")
+      ("minclip,c", boost::program_options::value<uint32_t>(&c.minClip)->default_value(100), "min. clipping length")
       ("uncertain,u", boost::program_options::value<uint32_t>(&c.bpuncertain)->default_value(1000), "breakpoint uncertainty (in bp)")
       ("sample,s", boost::program_options::value<std::string>(&c.sample)->default_value("NA12878"), "sample name (as in VCF/BCF file)")
       ("vcffile,v", boost::program_options::value<boost::filesystem::path>(&c.vcffile), "input VCF/BCF file")
