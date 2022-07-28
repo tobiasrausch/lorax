938086c0982e9bc7eaef1f92ac1e6f0d54c97e7b
tobiasrausch <rauschtobi@gmail.com>
2022-07-28 11:02:42 +0200

    ecDNA

--- src/ecc.h
+++ src/ecc.h
@@ -58,6 +58,23 @@ namespace lorax
     }
   };
 
+  struct CircleSegment {
+    int32_t cid;
+    int32_t tid;
+    int32_t gstart;
+    int32_t gend;
+    int32_t rstart;
+    int32_t rend;
+    bool fwd;
+    uint16_t qual;
+    std::size_t seed;
+    std::string qname;
+
+    CircleSegment(int32_t const cd, int32_t const t, int32_t const gs, int32_t const ge, int32_t const rs, int32_t const re, bool const val, uint16_t const qval, std::size_t s, std::string const& qn) : cid(cd), tid(t), gstart(gs), gend(ge), rstart(rs), rend(re), fwd(val), qual(qval), seed(s), qname(qn) {}
+  };
+
+  
+  
   inline int32_t
   overlap(std::vector<ReadMatch> const& candidates, int32_t const i, int32_t const j) {
     if (candidates[i].tid != candidates[j].tid) return 0;
@@ -68,8 +85,166 @@ namespace lorax
     coord[2] = candidates[j].gstart;
     coord[3] = candidates[j].gend;
     std::sort(coord.begin(), coord.end());
+
+    // Contained segments cannot be
+    if ((coord[1] == candidates[i].gstart) && (coord[2] == candidates[i].gend)) return 0;
+    if ((coord[1] == candidates[j].gstart) && (coord[2] == candidates[j].gend)) return 0;
+
+    // Overlap must be in expected order
+    if (candidates[i].fwd) {
+      // both forward
+      if (candidates[i].begalign) {
+	if (candidates[i].gstart < candidates[j].gstart) return 0;
+      } else {
+	if (candidates[j].gstart < candidates[i].gstart) return	0;
+      }
+    } else {
+      // both reverse
+      if (candidates[i].begalign) {
+	if (candidates[i].gend > candidates[j].gend) return 0;
+      } else {
+	if (candidates[j].gend > candidates[i].gend) return 0;
+      }
+    }
+
+    // Return overlap size
     return coord[2] - coord[1];
   }
+
+
+  template<typename TConfig>
+  inline void
+  circleSegments(TConfig const& c, std::string const& filename, std::set<std::size_t> const& candidates, std::vector<CircleSegment>& mp, std::vector<CircleSegment> const& ctrl) {
+    // For the tumor run
+    std::set<std::size_t> invalidReads;
+    bool tumor_run = false;
+    if (filename == c.tumor.string()) tumor_run = true;
+
+    // Open file handles
+    samFile* samfile = sam_open(filename.c_str(), "r");
+    hts_set_fai_filename(samfile, c.genome.string().c_str());
+    hts_idx_t* idx = sam_index_load(samfile, filename.c_str());
+    bam_hdr_t* hdr = sam_hdr_read(samfile);
+    
+    // Parse BAM
+    for(int32_t refIndex = 0; refIndex < hdr->n_targets; ++refIndex) {
+      boost::posix_time::ptime now = boost::posix_time::second_clock::local_time();
+      std::cout << '[' << boost::posix_time::to_simple_string(now) << "] Processing... " << hdr->target_name[refIndex] << std::endl;
+
+      // Mask control regions
+      typedef boost::dynamic_bitset<> TBitSet;
+      TBitSet mask;
+      if (tumor_run) {
+	mask.resize(hdr->target_len[refIndex], 0);
+	for(uint32_t i = 0; i < ctrl.size(); ++i) {
+	  if (ctrl[i].tid == refIndex) {
+	    for(int32_t k = ctrl[i].gstart; ((k < ctrl[i].gend) && (k < (int) hdr->target_len[refIndex])); ++k) mask[k] = 1;
+	  }
+	}
+      }
+      
+      // Iterate alignments 
+      hts_itr_t* iter = sam_itr_queryi(idx, refIndex, 0, hdr->target_len[refIndex]);
+      bam1_t* rec = bam_init1();
+      while (sam_itr_next(samfile, iter, rec) >= 0) {
+	if (rec->core.flag & (BAM_FQCFAIL | BAM_FDUP | BAM_FUNMAP | BAM_FSECONDARY)) continue;
+	std::size_t seed = hash_string(bam_get_qname(rec));
+	if (candidates.find(seed) != candidates.end()) {
+	  
+	  // Parse CIGAR
+	  uint32_t* cigar = bam_get_cigar(rec);
+	  int32_t gp = rec->core.pos; // Genomic position
+	  int32_t gpStart = -1; //Match start
+	  int32_t gpEnd = -1; //Match end
+	  int32_t sp = 0; // Sequence position
+	  int32_t seqStart = -1;  // Match start
+	  int32_t seqEnd = -1; // Match end
+	  for (std::size_t i = 0; i < rec->core.n_cigar; ++i) {
+	    if ((bam_cigar_op(cigar[i]) == BAM_CMATCH) || (bam_cigar_op(cigar[i]) == BAM_CEQUAL) || (bam_cigar_op(cigar[i]) == BAM_CDIFF)) {
+	      if (seqStart == -1) {
+		seqStart = sp;
+		gpStart = gp;
+	      }
+	      gp += bam_cigar_oplen(cigar[i]);
+	      sp += bam_cigar_oplen(cigar[i]);
+	      seqEnd = sp;
+	      gpEnd = gp;
+	    } else if (bam_cigar_op(cigar[i]) == BAM_CINS) {
+	      if (seqStart == -1) {
+		seqStart = sp;
+		gpStart = gp;
+	      }
+	      sp += bam_cigar_oplen(cigar[i]);
+	      seqEnd = sp;
+	      gpEnd = gp;
+	    } else if (bam_cigar_op(cigar[i]) == BAM_CDEL) {
+	      if (seqStart == -1) {
+		seqStart = sp;
+		gpStart = gp;
+	      }
+	      gp += bam_cigar_oplen(cigar[i]);
+	      seqEnd = sp;
+	      gpEnd = gp;
+	    } else if (bam_cigar_op(cigar[i]) == BAM_CSOFT_CLIP) {
+	      sp += bam_cigar_oplen(cigar[i]);
+	    } else if (bam_cigar_op(cigar[i]) == BAM_CREF_SKIP) {
+	      gp += bam_cigar_oplen(cigar[i]);
+	    } else if (bam_cigar_op(cigar[i]) == BAM_CHARD_CLIP) {
+	      sp += bam_cigar_oplen(cigar[i]);
+	    } else {
+	      std::cerr << "Warning: Unknown Cigar options!" << std::endl;
+	    }
+	  }
+
+	  // Alignment direction
+	  bool fwd = true;
+	  if (rec->core.flag & BAM_FREVERSE) {
+	    fwd = false;
+	    int32_t seqTmp = seqStart;
+	    seqStart = sp - seqEnd;
+	    seqEnd = sp - seqTmp;
+	  }
+	  if (gpStart < gpEnd) {
+	    bool insertSegment = true;
+	    if (tumor_run) {
+	      // For the tumor, check that mappings are absent in the control
+	      for(int32_t k = gpStart; ((k < gpEnd) && (k < (int) hdr->target_len[refIndex])); ++k) {
+		if (mask[k]) {
+		  insertSegment = false;
+		  invalidReads.insert(seed);
+		  break;
+		}
+	      }
+	    }
+	    //std::cerr << bam_get_qname(rec) << ',' << hdr->target_name[rec->core.tid] << ',' << gpStart << std::endl;
+	    if (insertSegment) mp.push_back(CircleSegment(mp.size(), rec->core.tid, gpStart, gpEnd, seqStart, seqEnd, fwd, rec->core.qual, seed, bam_get_qname(rec)));
+	  }
+	}
+      }
+      bam_destroy1(rec);
+      hts_itr_destroy(iter);
+    }
+
+    // Filter reads masked by control
+    if (invalidReads.size()) {
+    }
+
+    // Debug
+    std::cerr << invalidReads.size() << std::endl;
+    if (tumor_run) {
+      for(uint32_t i = 0; i < mp.size(); ++i) {
+	if (invalidReads.find(mp[i].seed) == invalidReads.end()) {
+	  std::cerr << hdr->target_name[mp[i].tid] << '\t' << mp[i].gstart << '\t' << mp[i].gend << '\t' << mp[i].qname << '\t' << mp[i].rstart << '\t' << mp[i].rend << '\t' << (int) mp[i].fwd << std::endl;
+	}
+      }
+    }
+    
+    // Clean-up
+    bam_hdr_destroy(hdr);
+    hts_idx_destroy(idx);
+    sam_close(samfile);
+  }
+
   
   template<typename TConfig>
   inline int32_t
@@ -223,6 +398,15 @@ namespace lorax
     int32_t csize = circleCandidates(c, c.control.string(), creads);
     std::cerr << tsize << ',' << csize << std::endl;
 
+    // Control regions
+    std::vector<CircleSegment> ctrlsegments;
+    std::vector<CircleSegment> tumsegments;
+    circleSegments(c, c.control.string(), creads, ctrlsegments, tumsegments);
+
+    // Tumor somatic eccDNA
+    circleSegments(c, c.tumor.string(), treads, tumsegments, ctrlsegments);
+
+    
 #ifdef PROFILE
     ProfilerStop();
 #endif
