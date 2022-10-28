b06cb4301d80bb2ff6bcea1d99f04a1fff554145
Tobias Rausch <rauschtobi@gmail.com>
2022-10-28 10:24:35 +0200

    align records

--- src/pct.h
+++ src/pct.h
@@ -162,6 +162,130 @@ namespace lorax
 
   template<typename TConfig>
   inline void
+  pctGaf(TConfig const& c) {
+    // Output file
+    std::ofstream ofile;
+    std::string fname = c.outprefix + ".alignments.tsv";
+    ofile.open(fname.c_str(), std::ofstream::out | std::ofstream::trunc);
+    ofile << "qname\tqlen\tqsublen\tpctidentity\tlargestdel\tmapped\tmatches\tmismatches\tdeletions\tdelsize\tinsertions\tinssize\tsoftclips\tsoftclipsize\thardclips\thardclipsize" << std::endl;
+    
+    // Parse GAF
+    std::ifstream gafFile(c.sample.string().c_str(), std::ios_base::in);
+    if(gafFile.is_open()) {
+      while (gafFile.good()) {
+	std::string gline;
+	getline(gafFile, gline);
+	AlignRecord ar;
+		   
+	typedef boost::tokenizer< boost::char_separator<char> > Tokenizer;
+	boost::char_separator<char> sep("\t");
+	Tokenizer tokens(gline, sep);
+	Tokenizer::iterator tokIter = tokens.begin();
+	if (tokIter != tokens.end()) {
+	  std::string qname = *tokIter;
+	  ar.seed = hash_lr(qname);
+	  ++tokIter;
+	  if (tokIter != tokens.end()) {
+	    ar.qlen = boost::lexical_cast<int32_t>(*tokIter);
+	    ++tokIter;
+	    if (tokIter != tokens.end()) {
+	      ar.qstart = boost::lexical_cast<int32_t>(*tokIter);
+	      ++tokIter;
+	      if (tokIter != tokens.end()) {
+		ar.qend = boost::lexical_cast<int32_t>(*tokIter);
+		++tokIter;
+		if (tokIter != tokens.end()) {
+		  ar.strand = boost::lexical_cast<char>(*tokIter);
+		  ++tokIter;
+		  if (tokIter != tokens.end()) {
+		    // Skip path parsing
+		    ++tokIter;
+		    if (tokIter != tokens.end()) {
+		      ar.plen = boost::lexical_cast<int32_t>(*tokIter);
+		      ++tokIter;
+		      if (tokIter != tokens.end()) {
+			ar.pstart = boost::lexical_cast<int32_t>(*tokIter);
+			++tokIter;
+			if (tokIter != tokens.end()) {
+			  ar.pend = boost::lexical_cast<int32_t>(*tokIter);
+			  ++tokIter;
+			  if (tokIter != tokens.end()) {
+			    ar.matches = boost::lexical_cast<int32_t>(*tokIter);
+			    ++tokIter;
+			    if (tokIter != tokens.end()) {
+			      ar.alignlen = boost::lexical_cast<int32_t>(*tokIter);
+			      ++tokIter;
+			      if (tokIter != tokens.end()) {
+				ar.mapq = boost::lexical_cast<int32_t>(*tokIter);
+				++tokIter;
+				for(; tokIter != tokens.end(); ++tokIter) {
+				  // Optional fields
+				  boost::char_separator<char> kvsep(":");
+				  Tokenizer tokens(*tokIter, kvsep);
+				  Tokenizer::iterator tikv = tokens.begin();
+				  if (*tikv == "cg") {
+				    ++tikv; ++tikv;
+				    parseGafCigar(*tikv, ar);
+				  }
+				}
+
+				
+				// Evaluate alignment record
+				uint32_t largestdel = 0;
+				uint32_t mismatch = 0;
+				uint32_t match = 0;
+				uint32_t del = 0;
+				uint32_t delsize = 0;
+				uint32_t ins = 0;
+				uint32_t inssize = 0;
+				uint32_t sc = 0;
+				uint32_t scsize = 0;
+				uint32_t hc = 0;
+				uint32_t hcsize = 0;
+				for (uint32_t i = 0; i < ar.cigarop.size(); ++i) {
+				  if (ar.cigarop[i] == BAM_CEQUAL) match += ar.cigarlen[i];
+				  else if (ar.cigarop[i] == BAM_CDIFF) mismatch += ar.cigarlen[i];
+				  else if (ar.cigarop[i] == BAM_CDEL) {
+				    if (ar.cigarlen[i] > largestdel) largestdel = ar.cigarlen[i];
+				    ++del;
+				    delsize += ar.cigarlen[i];
+				  }
+				  else if (ar.cigarop[i] == BAM_CINS) {
+				    ++ins;
+				    inssize += ar.cigarlen[i];
+				  }
+				  else {
+				    std::cerr << "Warning: Unknown Cigar option " << ar.cigarop[i] << std::endl;
+				    exit(-1);
+				  }
+				}
+				
+				// Percent identity
+				int32_t qsublen = ar.qend - ar.qstart;
+				double pctval = (double) (match) / (double) qsublen;
+				ofile << qname << '\t' << ar.qlen << '\t' << qsublen << '\t' << pctval << '\t' << largestdel << "\taligned\t" << match << '\t' << mismatch << '\t' << del << '\t' << delsize << '\t' << ins << '\t' << inssize << '\t' << sc << '\t' << scsize << '\t' << hc << '\t' << hcsize << std::endl;
+			      }
+			    }
+			  }
+			}
+		      }
+		    }
+		  }
+		}
+	      }
+	    }
+	  }
+	}
+      }
+      gafFile.close();
+    }
+    ofile.close();
+  }
+
+
+  
+  template<typename TConfig>
+  inline void
   pctBam(TConfig const& c) {
     // Open file handles
     samFile* samfile = sam_open(c.sample.string().c_str(), "r");
@@ -322,14 +446,13 @@ namespace lorax
 
     if (c.gfaMode) {
       // Load pan-genome graph
-      std::cout << '[' << boost::posix_time::to_simple_string(boost::posix_time::second_clock::local_time()) << "] Load pan-genome graph" << std::endl;
-      Graph g;
-      parseGfa(c, g);
+      //std::cout << '[' << boost::posix_time::to_simple_string(boost::posix_time::second_clock::local_time()) << "] Load pan-genome graph" << std::endl;
+      //Graph g;
+      //parseGfa(c, g);
 
       // Parse alignments
       std::cout << '[' << boost::posix_time::to_simple_string(boost::posix_time::second_clock::local_time()) << "] Parse alignments" << std::endl;
-      std::vector<AlignRecord> aln;
-      parseGaf(c, g, aln);
+      pctGaf(c);
 
       // Parse reads
       std::cout << '[' << boost::posix_time::to_simple_string(boost::posix_time::second_clock::local_time()) << "] Parse reads" << std::endl;
